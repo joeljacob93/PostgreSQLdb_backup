6db06e63a093f302086a20955bd88de471050834
Joel Jacob <joel.9320@gmail.com>
2020-09-29 04:21:52 +0000

    Structure project with setup.py and Makefile

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
+
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Database backups locally or to AWS S3.',
+    long_description=readme,
+    author='Keith',
+    author_email='keith@linuxacademy.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+)
